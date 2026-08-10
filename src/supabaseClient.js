1500b18a90cf5bf265f009c401f927157aa9d65c
PrashantDhiman005 <prashantdhiman104@gmail.com>
2026-08-10 11:50:56 +0530

    Update Supabase authentication

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,6 @@
 // src/services/supabase.js
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://cgrgoxrgxcyldadnrcrv.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNncmdveHJneGN5bGRhZG5yY3J2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTIwNzU3NDgsImV4cCI6MjA2NzY1MTc0OH0.4X9f0uAFKQlrBnb4WQhl-FT7Wmrwbxfly-6M2NFdzvo'
+const supabaseUrl = 'https://xmlhsaiqkssxxyumkbva.supabase.co'
+const supabaseKey = 'sb_publishable_TfFNGPRIrdkMt7tr6EAkPA_XLPklm_i'
 export const supabase = createClient(supabaseUrl, supabaseKey)
